aaf90c21399112bb2ed0b82f4e65f852a9b1c37c
RIKO-99 <princericko6@gmal.com>
2022-09-12 01:44:30 -0700

    task 7 and 8

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/*
+ * main - Entry point
+ *
+ * print lower case alphabets in reverse
+ *
+ * Return: 0 Always (Success)
+ *
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 'z'; n >= 'a'; n--)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
