b28432862a043ad7b9e15837ed77f6d80a5caf52
RIKO-99 <princericko6@gmal.com>
2022-09-12 12:43:35 -0700

    gcc world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * prints the sentence in the standard error
+ *
+ * Return: 0 Always (Success)
+ *
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
