9fdc770cfe2aa9c31d23b0ed1ebaa7ae79667654
RIKO-99 <princericko6@gmal.com>
2022-09-12 13:51:45 -0700

    last digit retry

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,15 +13,17 @@
 int main(void)
 {
 	int n;
+	int last;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("Last digit of %d is %d ", n, n % 10);
-	if (n % 10 > 5)
-		printf("and is greater than 5\n");
-	else if (n % 10 == 0)
-		printf("and is greater than 5\n");
-	else if (n % 10 < 6 && n % 10 != 0)
-		printf("and is less than 6 and not 0\n");
+	last = n % 10;
+
+	if (last > 5)
+		printf("last digit of %i is %i and is greater than 5\n", n, last);
+	else if (last == 0)
+		printf("last digit of %i is %i and is greater than 0\n", n, last);
+	else if (last < 6 && last != 0)
+		printf("last digit of %i is %i and is less than 6 and not 0\n", n, last);
 	return (0);
 }
