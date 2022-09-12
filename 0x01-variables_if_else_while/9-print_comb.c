eed86ac7b329d69ded4953847ef79e0ede3cb2e8
RIKO-99 <princericko6@gmal.com>
2022-09-12 02:16:19 -0700

    combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * prints all possible combination
+ *
+ * Return: 0 Always (Success)
+ *
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
