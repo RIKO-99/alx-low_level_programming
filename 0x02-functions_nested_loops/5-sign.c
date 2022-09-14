6afe13ab6d538448d884965b3eb2ea8fd3a722a9
RIKO-99 <princericko6@gmal.com>
2022-09-14 14:48:43 -0700

    task 5 (2)

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -16,15 +16,15 @@ int print_sign(int n)
 		test = 1;
 		_putchar('+');
 	}
-	else if (n < 0)
-	{
-		test = -1;
-		_putchar('-');
-	}
-	else if
+	else if (n == 0)
 	{
 		test = 0;
 		_putchar(0);
+	}
+	else 
+	{
+		test = -1;
+		_putchar('-');
 	}
 	return (test);
 }
