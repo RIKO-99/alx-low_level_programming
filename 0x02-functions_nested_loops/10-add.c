b979dfa39e7b52416de1b613dedb96035669754f
RIKO-99 <princericko6@gmal.com>
2022-09-14 16:36:06 -0700

    sum of integer

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - function adds two integers
+ * @x: integer 1
+ * @y: integer 2
+ * Return: Always 0
+ *
+ */
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+
+	return (sum);
+}
