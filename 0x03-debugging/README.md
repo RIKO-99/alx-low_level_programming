7cfd43f4831bcbfdffed7be38ed3b413278f778e
RIKO-99 <princericko6@gmal.com>
2022-09-15 04:41:00 -0700

    Debug mode

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+Building Debugging muscle in C
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * File - main.h
+ *
+ * Description : Header file containing function prototypes
+ *
+ */
+void positive_or_negative(int i);
+
+#endif /*MAIN_H*/
