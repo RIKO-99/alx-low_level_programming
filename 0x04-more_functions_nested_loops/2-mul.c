6ee3f1229cdac5b76387175223c9387c8890b889
RIKO-99 <princericko6@gmal.com>
2022-09-16 05:17:03 -0700

    multiples

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - checks the product of two number
+ *
+ * @a: integer
+ * @b: integer
+ *
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
