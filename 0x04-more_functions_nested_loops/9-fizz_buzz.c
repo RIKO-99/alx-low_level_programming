e85ba0e22c69714225f895d20664d3cc0b79895c
RIKO-99 <princericko6@gmal.com>
2022-09-19 01:43:07 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,13 +7,11 @@
  */
 int main(void)
 {
-	int n;
+	int n = 1;
 
-	n = 1;
-	printf("%d", n);
-	for (n = 2; n <= 100; n++)
+	for (; n <= 100; n++)
 	{
-		if ((n % 3 == 0) && (n % 5 == 0))
+		if (n % 3 == 0 && n % 5 == 0)
 		{
 			printf(" FizzBuzz");
 		}
