a46f7d24996408dbbac905887a867834cacf51da
RIKO-99 <princericko6@gmal.com>
2022-09-23 22:22:28 -0700

    numbers have life

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: inputs integer
+ *
+ * Return: 0 Always
+ *
+ */
+
+void print_number(int n)
+{
+	unsigned int num;
+
+	num = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		num = -n;
+	}
+	if (num / 10)
+	{
+		print_number(num / 10);
+	}
+	_putchar((num % 10) + '0');
+}
