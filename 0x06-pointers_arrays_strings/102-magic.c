3382f014580c3d764c9d9a021b72e5b9b24836e6
RIKO-99 <princericko6@gmal.com>
2022-09-23 21:05:33 -0700

    voila this is Magic

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Block
+ *
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+
+	/*
+	 * code goes here
+	 * only one statement to be changed
+	 */
+	p[5] = 98;
+	/* prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
