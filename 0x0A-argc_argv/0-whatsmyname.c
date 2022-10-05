b9528dc210150736a7c4dffc4e370d818daca374
RIKO-99 <princericko6@gmal.com>
2022-10-05 02:04:23 -0700

    getting started with arguments to main

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - program prints its name
+ * @agrc: arguments count
+ * @argv: pointer to string
+ *
+ * Return: 0 Always (Success)
+ */
+
+int main(int agrc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
