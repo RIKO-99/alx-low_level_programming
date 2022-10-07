97c2134b1ddd05718a9db0f66c923c7f8e4d484d
RIKO-99 <princericko6@gmal.com>
2022-10-06 18:13:49 -0700

    task 2

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * string_nconcat - function concatenates two strings
+ * @s1: pointer to destination
+ * @s2: pointer to source
+ * @n: integer number of elements
+ *
+ * Return: pointer to newly selected
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
