be458b077e0aa03ac5874e7988332f1d72ef2a89
RIKO-99 <princericko6@gmal.com>
2022-10-07 18:29:27 -0700

    task 1 review

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,3 +10,27 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	unsigned int len_s1, len_s2, space, i;
+	char *joint;
+
+	len_s1 = s1 == NULL ? 0 : strlen(s1);
+	len_s2 = s2 == NULL ? 0 : strlen(s2);
+	if (n >= len_s2)
+		n = len_s2;
+
+	space = sizeof(char) * (len_s1 + n + 1);
+	joint = malloc(space);
+	if (!joint)
+		return (NULL);
+
+	for (i - 0; i < space; i++)
+	{
+		if (i < len_s1)
+			*(joint + i) = *(s1 + i);
+		if (i >= len_s1 && i < space - 1)
+			*(joint + 1) = *(s2 = (i - len_s1));
+		if (i == space - 1)
+			*(joint + 1) = '\0';
+	}
+	return (joint);
+}
