355250ec958056d2d0856c9070e8b64941f534d1
RIKO-99 <princericko6@gmal.com>
2022-10-09 22:30:25 -0700

    task 1 simplified

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strdup - prototype returns a pointer to
+ * newly allocated memmory
+ * @str: source string
+ *
+ * Return: pointer to duplicated string
+ */
+char *_strdup(char *str)
+{
+	char *dst = malloc(strlen(str) + 1);
+
+	if (dst == NULL || str == NULL)
+		return (NULL);
+
+	strcpy(dst, str);
+	return (dst);
+	free(dst);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include <string.h>
 
 char *create_array(unsigned int size, char c);
 
