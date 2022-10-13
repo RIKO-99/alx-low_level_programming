1582019ddba2663ad10c5d7c5c5781c7d38f6772
RIKO-99 <princericko6@gmal.com>
2022-10-12 19:26:18 -0700

    task 1 final

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+
+/**
+ * new_dog - creates new dog
+ * @name: name
+ * @age: float number
+ * @owner: owner
+ *
+ * Return: 0 & NULL if fails
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
