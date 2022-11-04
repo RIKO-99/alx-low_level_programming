04264e48823c528881ceaf779b9632f2e4176513
RIKO-99 <princericko6@gmal.com>
2022-11-04 08:03:04 -0700

    task 1 done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,8 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = '\0';
 
-	for (i = 0; text_content[i]; i++);
+	for (i = 0; text_content[i]; i++)
+		;
 
 	rwr = write(fd, text_content, i);
 
